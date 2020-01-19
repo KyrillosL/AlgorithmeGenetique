3dc2a2761432bc21097cfd5cd2ae83c4948d2220
f <f>
2020-01-19 23:49:52 +0100

    realtime plot 2

--- algorithme_genetique.py
+++ algorithme_genetique.py
@@ -31,25 +31,83 @@ class Algorithme_genetique:
 
     def solve(self):
 
+        #lo = op_sel.upper_confidence_bound(self.population, 0.1, 0.9, 0.05)
+        #lo = op_sel.upper_confidence_bound(self.population)
+        #lo = op_sel.best_operator(self.population)
+        #lo = op_sel.roulette_adaptive(self.population)
+        #lo = op_sel.roulette_fixe(self.population)
+        lo = op_sel.adaptive_pursuit(self.population)
+        nb_op = len(lo.list_operators)
 
-        fig = plt.figure()
+
+        fig = plt.figure(figsize=(20, 10))
+        #SCORE PLOT
         plt_score= fig.add_subplot(3, 2, 1)
-        plt_score.set_xlim(0,1000)
         plt_score.set_ylim([0, 1.1])
         plt_score.set_ylabel('Score')
         plt_score.set_xlabel('Generation')
+        line_score, = plt_score.plot([], lw=2, c='black')
 
-        line_score, = plt_score.plot([], lw=3, c='black')
+        list_plt_prob=[]
+        list_line_plt=[]
+
+        for i in range(nb_op):
+            plt_probs =fig.add_subplot(3, 2, i+2)
+
+            plt_probs.set_xlabel('Generation')
+
+
+            if i==0:
+                line, = plt_probs.plot([], lw=1, c='red')
+                plt_probs.set_ylabel('flip 1_n')
+            if i == 1:
+                line, = plt_probs.plot([], lw=1, c='green')
+                plt_probs.set_ylabel('flip 3_n')
+            if i == 2:
+                line, = plt_probs.plot([], lw=1, c='blue')
+                plt_probs.set_ylabel('flip 5_n')
+            list_line_plt.append(line)
+            list_plt_prob.append(plt_probs )
+
+
+        '''
+        #PROBS PLOT
+        plt_probs= fig.add_subplot(2, 1, 2)
+        plt_probs.set_ylim([0, 1.1])
+        plt_probs.set_ylabel('Prob')
+        plt_probs.set_xlabel('Generation')
+        line_op_1, = plt_probs.plot([], lw=3, c='red')
+        line_op_2, = plt_probs.plot([], lw=3, c='green')
+        line_op_3, = plt_probs.plot([], lw=3, c='blue')
+        '''
 
 
         fig.canvas.draw()
         score_bg = fig.canvas.copy_from_bbox(plt_score.bbox)
+        #probs_bg = fig.canvas.copy_from_bbox(plt_probs.bbox)
+        list_op_bg=[]
+        list_list_data_prob=[]
+        for i in range(nb_op):
+            list_op_bg.append(fig.canvas.copy_from_bbox(list_plt_prob[i].bbox))
+            list_data_prob =[]
+            list_data_prob.append(0)
+            list_list_data_prob.append(list_data_prob)
+
+
 
 
         time=[]
         time.append(0)
+
         data_score=[]
         data_score.append(0)
+
+        data_probs_op_1=[]
+        data_probs_op_1.append(0)
+        data_probs_op_2=[]
+        data_probs_op_2.append(0)
+        data_probs_op_3=[]
+        data_probs_op_3.append(0)
 
 
         plt.show(block=False)
@@ -59,35 +117,51 @@ class Algorithme_genetique:
         iteration = 0
 
         temp_score = 0
-        #lo = op_sel.upper_confidence_bound(self.population, 0.1, 0.9, 0.05)
-        #lo = op_sel.upper_confidence_bound(self.population)
-        #lo = op_sel.best_operator(self.population)
-        #lo = op_sel.roulette_adaptive(self.population)
-        #lo = op_sel.roulette_fixe(self.population)
-        lo = op_sel.adaptive_pursuit(self.population)
 
 
         while self.population.select_best_agents(1).get(0).score() != 1.0 :
             lo.apply()
-
-            xmin, xmax, ymin, ymax = [min(time) / 1.05, max(time) * 1.1, 0, 1.1]
-            plt_score.axis([xmin, xmax, ymin, ymax])
-
-
             time.append(iteration)
+
+            #SCORE
+            xmin_score, xmax_score, ymin_score, ymax_score = [min(time) / 1.05, max(time) * 1.1, 0, 1.1]
+            plt_score.axis([xmin_score, xmax_score, ymin_score, ymax_score])
             data_score.append(self.population.select_best_agents(1).get(0).score())
-
             line_score.set_data(time,data_score )
-
             fig.canvas.restore_region(score_bg)
-
             plt_score.draw_artist(line_score)
-
-
             fig.canvas.blit(plt_score.bbox)
 
-            fig.canvas.flush_events()
 
+
+            for i in range(nb_op):
+                list_list_data_prob[i].append(lo.list_operators[i].probability)
+                xmin_probs, xmax_probs, ymin_probs, ymax_probs = [min(time) / 1.05, max(time) * 1.1, 0,max(list_list_data_prob[i])*1.1]
+                list_plt_prob[i].axis([xmin_probs, xmax_probs, ymin_probs, ymax_probs])
+                list_line_plt[i].set_data(time, list_list_data_prob[i] )
+                fig.canvas.restore_region( list_op_bg[i] )
+                list_plt_prob[i].draw_artist(list_line_plt[i])
+                fig.canvas.blit( list_plt_prob[i].bbox)
+
+            '''
+            #PROBS
+            data_probs_op_1.append(lo.list_operators[0].probability)
+            data_probs_op_2.append(lo.list_operators[1].probability)
+            data_probs_op_3.append(lo.list_operators[2].probability)
+            xmin_probs, xmax_probs, ymin_probs, ymax_probs = [min(time) / 1.05, max(time) * 1.1, 0, max(x.probability for x in lo.list_operators)]
+            plt_probs.axis([xmin_probs, xmax_probs, ymin_probs, ymax_probs])
+
+            line_op_1.set_data(time,data_probs_op_1 )
+            line_op_2.set_data(time, data_probs_op_2)
+            line_op_3.set_data(time, data_probs_op_3)
+            fig.canvas.restore_region(probs_bg)
+            plt_probs.draw_artist(line_op_1)
+            plt_probs.draw_artist(line_op_2)
+            plt_probs.draw_artist(line_op_3)
+            fig.canvas.blit(plt_probs.bbox)
+            '''
+
+            fig.canvas.flush_events()
 
 
             print(self.population.select_best_agents(1).get(0).score())
